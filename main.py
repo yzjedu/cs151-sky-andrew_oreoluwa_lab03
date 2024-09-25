dab61dd318e2221d6b3556960fb79940429271f3
Ms_Gigi <ogadebusoye@loyola.edu>
2024-09-25 16:23:56 -0400

    will continue

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-# Enter Code here
-# Read the README - under files on left
-# Delete these three lines before submitting
+# Programmers: Oreoluwa Adebusoye & Andrew Leimbach
+# Course:  CS151, Dr. Zelalem Jembre Yalew
+# Due Date: 10/02/2024
+# Lab Assignment: 03
+# Problem Statement: Winter is coming! One winter sport is the ski jump, where the score is determined by the distance traveled after skiing down a ramp and into the air.
+#                    What type of speed does a ski jumper need to achieve on the ramp to make a good distance on their jump?
+# Purpose: Given the type of ski jump (normal vs. large) and the jumper’s speed at the end of the ramp, this program predicts how far they will jump
+# Data In: Hill type, Speed
+# Data Out:
+# Credits: Used calculations provided in class
